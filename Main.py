c5f6a80e6d632760f1770e8a5116822675fbd816
Sandro <thesandro1998@gmail.com>
2020-06-27 19:35:02 +0400

    OOPification complete

--- Main.py
+++ Main.py
@@ -1,20 +1,12 @@
-import io
 import string
-from ctypes import Union
 
+import matplotlib.pyplot as plt
 import pandas as pd
-from pandas import Series, DataFrame
-from sklearn.tree import DecisionTreeClassifier
+from pandas import DataFrame
 from sklearn.model_selection import cross_val_score
-
+from sklearn.tree import DecisionTreeClassifier
 
 numberOfMatches = 150
-"""
-players = pd.read_csv('players.csv', nrows=10 * numberOfMatches)
-matches = pd.read_csv('match.csv', nrows=numberOfMatches)
-heroes = pd.read_csv('hero_names.csv')
-items = pd.read_csv('item_ids.csv')
-"""
 
 
 class PlayerFormatter:
@@ -50,101 +42,79 @@ class PlayerFormatter:
         self.players['item_5'] = self.players['item_5'].apply(find_item)
 
 
-"""
-hero_lookup = dict(zip(heroes['hero_id'], heroes['localized_name']))
-hero_lookup[0] = 'Unknown'
-players['hero'] = players['hero_id'].apply(lambda _id: hero_lookup[_id])
+class RadiantDireData:
+    radiant_heroes = []
+    dire_heroes = []
+    radiant_items = []
+    dire_items = []
 
-item_lookup = dict(zip(items['item_id'], items['item_name']))
-item_lookup[0] = 'Unknown'
+    def __init__(self, players: DataFrame):
+        player_heroes = pd.get_dummies(players['hero'])
+        item0 = pd.get_dummies(players['item_0'].fillna(0))
+        item1 = pd.get_dummies(players['item_1'].fillna(0))
+        item2 = pd.get_dummies(players['item_2'].fillna(0))
+        item3 = pd.get_dummies(players['item_3'].fillna(0))
+        item4 = pd.get_dummies(players['item_4'].fillna(0))
+        item5 = pd.get_dummies(players['item_5'].fillna(0))
+        player_items = item0 \
+            .add(item1, fill_value=0) \
+            .add(item2, fill_value=0) \
+            .add(item3, fill_value=0) \
+            .add(item4, fill_value=0) \
+            .add(item5, fill_value=0)
+
+        radiant_cols = list(map(lambda s: 'radiant_' + s, player_heroes.columns.values))
+        dire_cols = list(map(lambda s: 'dire_' + s, player_heroes.columns.values))
+        radiant_items_cols = list(map(lambda s: 'radiant_' + str(s), player_items.columns.values))
+        dire_items_cols = list(map(lambda s: 'dire_' + str(s), player_items.columns.values))
+
+        for _id, _index in players.groupby('match_id').groups.items():
+            self.radiant_heroes.append(player_heroes.iloc[_index][:5].sum().values)
+            self.dire_heroes.append(player_heroes.iloc[_index][5:].sum().values)
+            self.radiant_items.append(player_items.iloc[_index][:5].sum().values)
+            self.dire_items.append(player_items.iloc[_index][5:].sum().values)
+
+        self.radiant_heroes = pd.DataFrame(self.radiant_heroes, columns=radiant_cols)
+        self.dire_heroes = pd.DataFrame(self.dire_heroes, columns=dire_cols)
+        self.radiant_items = pd.DataFrame(self.radiant_items, columns=radiant_items_cols)
+        self.dire_items = pd.DataFrame(self.dire_items, columns=dire_items_cols)
 
 
-def find_item(_id):
-    return item_lookup.get(_id, 'u_' + str(_id))
+class DecisionTree:
+    X: DataFrame
+    Y: DataFrame
+    dt: DecisionTreeClassifier
 
+    def __init__(self, X, Y):
+        self.X = X
+        self.Y = Y
+        self.build_decision_tree()
 
-players['item_0'] = players['item_0'].apply(find_item)
-players['item_1'] = players['item_1'].apply(find_item)
-players['item_2'] = players['item_2'].apply(find_item)
-players['item_3'] = players['item_3'].apply(find_item)
-players['item_4'] = players['item_4'].apply(find_item)
-players['item_5'] = players['item_5'].apply(find_item)
-print(players['item_0'])
-"""
+    def build_decision_tree(self):
+        self.dt = DecisionTreeClassifier()
+        self.dt.fit(self.X, self.Y)
+
+    def print_cross_validation(self):
+        print('CV score:', cross_val_score(estimator=self.dt, X=self.X, y=self.Y).mean())
+
+    def get_tree_stats(self):
+        feature_importance = dict(zip(X.columns, self.dt.feature_importances_))
+        feature_importance = pd.Series(feature_importance).sort_values(ascending=False)
+        return feature_importance.head(20)
+
 
 players = PlayerFormatter("players.csv", "hero_names.csv", "item_ids.csv").players
+data = RadiantDireData(players)
 
-player_heroes = pd.get_dummies(players['hero'])
-
-item0 = pd.get_dummies(players['item_0'].fillna(0))
-item1 = pd.get_dummies(players['item_1'].fillna(0))
-item2 = pd.get_dummies(players['item_2'].fillna(0))
-item3 = pd.get_dummies(players['item_3'].fillna(0))
-item4 = pd.get_dummies(players['item_4'].fillna(0))
-item5 = pd.get_dummies(players['item_5'].fillna(0))
-player_items = item0 \
-    .add(item1, fill_value=0) \
-    .add(item2, fill_value=0) \
-    .add(item3, fill_value=0) \
-    .add(item4, fill_value=0) \
-    .add(item5, fill_value=0)
-
-radiant_cols = list(map(lambda s: 'radiant_' + s, player_heroes.columns.values))
-dire_cols = list(map(lambda s: 'dire_' + s, player_heroes.columns.values))
-radiant_items_cols = list(map(lambda s: 'radiant_' + str(s), player_items.columns.values))
-dire_items_cols = list(map(lambda s: 'dire_' + str(s), player_items.columns.values))
-
-X = None
-
-# if isfile('mapped_match_hero_item.csv'):
-#    X = pd.read_csv('mapped_match_hero_item.csv')
-# else:
-
-radiant_heroes = []
-dire_heroes = []
-radiant_items = []
-dire_items = []
-
-for _id, _index in players.groupby('match_id').groups.items():
-    radiant_heroes.append(player_heroes.iloc[_index][:5].sum().values)
-    dire_heroes.append(player_heroes.iloc[_index][5:].sum().values)
-    radiant_items.append(player_items.iloc[_index][:5].sum().values)
-    dire_items.append(player_items.iloc[_index][5:].sum().values)
-
-radiant_heroes = pd.DataFrame(radiant_heroes, columns=radiant_cols)
-dire_heroes = pd.DataFrame(dire_heroes, columns=dire_cols)
-radiant_items = pd.DataFrame(radiant_items, columns=radiant_items_cols)
-dire_items = pd.DataFrame(dire_items, columns=dire_items_cols)
-X = pd.concat([radiant_heroes, radiant_items, dire_heroes, dire_items], axis=1)
-matches = pd.read_csv('match.csv', nrows=numberOfMatches)
+X = pd.concat([data.radiant_heroes, data.radiant_items, data.dire_heroes, data.dire_items], axis=1)
 X.to_csv('mapped_match_hero_item.csv', index=False)
-y = matches['radiant_win'].apply(lambda win: 1 if win else 0)
-classes = ['Dire Win', 'Radiant Win']
 
+matches = pd.read_csv('match.csv', nrows=numberOfMatches)
+Y = matches['radiant_win'].apply(lambda win: 1 if win else 0)
 
-# jemali = pd.Series(X).apply(lambda i: item_lookup[i]).value_counts().plot(kind='bar')
-# plt.show()
-# _ = pd.Series(y).apply(lambda i: classes[i]).value_counts().plot('bar')
+dt = DecisionTree(X, Y)
+stats = dt.get_tree_stats()
 
-
-def build_decision_tree(X, y):
-    dt = DecisionTreeClassifier()
-    print('CV score:', cross_val_score(estimator=dt, X=X, y=y).mean())
-    dt.fit(X, y)
-    return dt
-
-
-dt = build_decision_tree(X=X, y=y)
-
-
-def get_tree_stats(dts):
-    feature_importances = dict(zip(X.columns, dts.feature_importances_))
-    feature_importances = pd.Series(feature_importances).sort_values(ascending=False)
-    return feature_importances.head(20)
-
-
-stats = get_tree_stats(dts=dt)
 stats.plot()
-import matplotlib.pyplot as plt
 
 plt.show()
